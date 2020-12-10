948565a2e491c366f5cb2446a03f7702ef0509ea
Tony Benoy <me@tonybenoy.com>
2020-12-11 01:59:50 +0530

    pipeline

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -15,8 +15,12 @@ class TestConfig(object):
     DEBUG = True
     SECRET_KEY = os.environ.get("SECRET_KEY") or "APPPASSWORD"
     SQLALCHEMY_DATABASE_URI = (
-        "postgresql+psycopg2://postgres:postgres@postgres:5432/loanmanagementtest"
+        "postgresql+psycopg2://postgres:postgres@localhost:5432/loanmanagementtest"
     )
+    # Incase you need to through compose
+    # SQLALCHEMY_DATABASE_URI = (
+    #     "postgresql+psycopg2://postgres:postgres@postgres:5432/loanmanagementtest"
+    # )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     API_VERSION = "0.0.1"
     API_FOR = "loanmanagement"
